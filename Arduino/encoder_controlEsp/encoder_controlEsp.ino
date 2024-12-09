bb84e48e6def2ac02de76a12779493bc6a2d82ec
Ihsan Fadilah <130231696+IhsanRobotik@users.noreply.github.com>
2024-12-09 20:47:09 +0100

    updated pcb

--- Arduino/encoder_controlEsp/encoder_controlEsp.ino
+++ Arduino/encoder_controlEsp/encoder_controlEsp.ino
@@ -2,8 +2,6 @@
 
 #define ENCODER_A 23
 #define ENCODER_B 22
-// #define ENCODER_A1 1
-// #define ENCODER_B1 3
 #define ENCODER_A2 19
 #define ENCODER_B2 18
 #define ENCODER_A3 5
@@ -18,7 +16,6 @@
 #define inA3 4
 #define inB3 0
 
-
 #define pwm0 32
 #define pwm1 33
 #define pwm2 25
@@ -29,7 +26,7 @@ int speed = 0;
 float filteredSpeed = 0;  
 
 double Setpoint, Input, Output;
-double Kp = 5, Ki = 4.2, Kd = 0.02;
+double Kp = 10, Ki = 5, Kd = 0.05;
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 volatile int encoder_value = 0;
@@ -63,10 +60,11 @@ void setup() {
   pinMode(ENCODER_B, INPUT_PULLUP);
   
   attachInterrupt(digitalPinToInterrupt(ENCODER_A), encoder_isr, CHANGE);
+
+  Serial.println("Enter PID values in the format: P I D");
 }
 
 void loop() {
-
   unsigned long currentTime = millis();
   
   if (currentTime - lastPrintTime >= 100) {
@@ -86,21 +84,57 @@ void loop() {
     Serial.println(filteredSpeed);
   }
 
-  run(0);
+  run(1);
+  handleSerialInput(); // Check for PID tuning input
 }
 
 void run(bool direction) {
   Input = filteredSpeed;  
-  if (direction == 0){
+  if (direction == 0) {
     digitalWrite(inA, HIGH);
     digitalWrite(inB, LOW);
     myPID.Compute();  
     analogWrite(pwm0, Output); 
-  }
-  else {
+  } else {
     digitalWrite(inA, LOW);
     digitalWrite(inB, HIGH);
     myPID.Compute();  
     analogWrite(pwm0, Output); 
+  }
+}
+
+// Function to handle Serial input for PID tuning
+void handleSerialInput() {
+  if (Serial.available()) {
+    String inputString = Serial.readStringUntil('\n'); // Read a line of input
+    inputString.trim(); // Remove leading/trailing whitespace
+
+    // Split input string into P, I, D values
+    int firstSpace = inputString.indexOf(' ');
+    int secondSpace = inputString.indexOf(' ', firstSpace + 1);
+
+    if (firstSpace > 0 && secondSpace > firstSpace) {
+      String kpStr = inputString.substring(0, firstSpace);
+      String kiStr = inputString.substring(firstSpace + 1, secondSpace);
+      String kdStr = inputString.substring(secondSpace + 1);
+
+      double newKp = kpStr.toDouble();
+      double newKi = kiStr.toDouble();
+      double newKd = kdStr.toDouble();
+
+      if (newKp >= 0 && newKi >= 0 && newKd >= 0) {
+        Kp = newKp;
+        Ki = newKi;
+        Kd = newKd;
+
+        // Update PID controller with new parameters
+        myPID.SetTunings(Kp, Ki, Kd);
+
+      } else {
+        Serial.println("Invalid PID values. Please enter positive numbers.");
+      }
+    } else {
+      Serial.println("Invalid format. Use: P I D");
+    }
   }
 }
